d4edced9ee4e63b94a73a80194edf675ac18a055
Maxim Filenkov <mvfilenkov@sberbank.ru>
2022-12-06 15:48:40 +0300

    поменял инициализацию интерактора

--- androidApp/src/main/java/com/favqsclient/kmm/android/login/presentation/LoginViewModelFactory.kt
+++ androidApp/src/main/java/com/favqsclient/kmm/android/login/presentation/LoginViewModelFactory.kt
@@ -2,7 +2,6 @@ package com.favqsclient.kmm.android.login.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.favqsclient.kmm.data.RepositoryImpl
 import com.favqsclient.kmm.domain.InteractorImpl
 
 /**
@@ -10,11 +9,8 @@ import com.favqsclient.kmm.domain.InteractorImpl
  *
  * @author Evgeny Chumak
  **/
-class LoginViewModelFactory(
-) : ViewModelProvider.Factory {
+class LoginViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val repository = RepositoryImpl()
-        val interactor = InteractorImpl.invoke(repository)
-        return LoginViewModel(interactor = interactor) as T
+        return LoginViewModel(interactor = InteractorImpl) as T
     }
 }
